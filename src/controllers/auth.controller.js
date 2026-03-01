3f196eff1234d6ddaff9c77016f1bda0795d209e
KEYURI <keyurijethava@gmail.com>
2026-03-01 11:33:58 +0530

    Add nodemailer for email functionality; implement email service and registration email feature

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,89 +1,102 @@
-const userModel=require('../models/users.model')
-const cookieParser=require('cookie-parser')
-const jwt =require('jsonwebtoken')
-
+const userModel = require("../models/users.model");
+const jwt = require("jsonwebtoken");
+const emailService = require("../services/email.service");
 
 /**
  * - user register controller
  * - POST /api/auth/register
  */
-const userRegisterController=async (req,res)=>{
-    const {email,name,password}=req.body
-   
-    const isExists=await userModel.findOne({
-        email:email
-    })
-    if(isExists){
-        return res.status(422).json({
-            message:"User already exists with email",
-            status: "failed"
-        })
-    }
+const userRegisterController = async (req, res) => {
+  const { email, name, password } = req.body;
 
-    const user =await userModel.create({
-        email,name,password
-    })
+  const isExists = await userModel.findOne({
+    email: email,
+  });
+  if (isExists) {
+    return res.status(422).json({
+      message: "User already exists with email",
+      status: "failed",
+    });
+  }
 
-    const token=jwt.sign({
-        userId:user._id
-    },process.env.JWT_SECRET,{expiresIn:"3d"})
+  const user = await userModel.create({
+    email,
+    name,
+    password,
+  });
 
-    res.cookie("token",token)
+  const token = jwt.sign(
+    {
+      userId: user._id,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "3d" },
+  );
 
-    res.status(201).json({
-        message:"User created successfully",
-        user:{
-            _id:user._id,
-            email:user.email,
-            name:user.name
-        },
-        token
-    })
-}
+  res.cookie("token", token);
+
+  res.status(201).json({
+    message: "User created successfully",
+    user: {
+      _id: user._id,
+      email: user.email,
+      name: user.name,
+    },
+    token,
+  });
+
+  await emailService.sendRegistrationEmail(user.email, user.name);
+};
 
 /**
  * - user login controller
  * - POST /api/auth/login
  */
 
-const userLoginController=async (req,res)=>{
-    const {email,password}=req.body
+const userLoginController = async (req, res) => {
+  const { email, password } = req.body;
 
-    const user=await userModel.findOne({
-        email
-    }).select("+password")
-
-    if(!user){
-       return res.status(401).json({
-        message:"Email or password is invalid"
-       })
-    }
-
-    const isValidPassword =await user.comparePassword(password)
-
-    if(!isValidPassword){
-        return res.status(401).json({
-        message:"Email or password is invalid"
-       })
-    }
-
-    const token=jwt.sign({
-        userId:user._id
-    },process.env.JWT_SECRET,{expiresIn:"3d"})
-
-    res.cookie("token",token)
-
-    res.status(200).json({
-        message:"User login successfully",
-        user:{
-            _id:user._id,
-            email:user.email,
-            name:user.name
-        },
-        token
+  const user = await userModel
+    .findOne({
+      email,
     })
-}
-module.exports={
-    userRegisterController,
-    userLoginController
-}
+    .select("+password");
+
+  if (!user) {
+    return res.status(401).json({
+      message: "Email or password is invalid",
+    });
+  }
+
+  const isValidPassword = await user.comparePassword(password);
+
+  if (!isValidPassword) {
+    return res.status(401).json({
+      message: "Email or password is invalid",
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      userId: user._id,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "3d" },
+  );
+
+  res.cookie("token", token);
+
+  res.status(200).json({
+    message: "User login successfully",
+    user: {
+      _id: user._id,
+      email: user.email,
+      name: user.name,
+    },
+    token,
+  });
+};
+module.exports = {
+  userRegisterController,
+  userLoginController,
+};
